ceb35abc573a794ee1b2d86509aae9c93b14be61
keithschumacher <keithschumacher.daily@gmail.com>
2025-05-16 11:48:49 -0700

    remove tool call engine args

--- serve.py
+++ serve.py
@@ -210,8 +210,6 @@ model_args = {
     "tensor-parallel-size": os.environ['TENSOR_PARALLELISM'], 
     "pipeline-parallel-size": os.environ['PIPELINE_PARALLELISM'],
     "dtype": os.environ.get('DTYPE', 'float16'),
-    "enable-auto-tool-choice": os.environ.get('ENABLE_AUTO_TOOL_CHOICE', 'false').lower() == 'true',
-    "tool-call-parser": os.environ.get('TOOL_CALL_PARSER', 'false').lower() == 'true'
 }
 
 # For backwards compatibility, keep the default 'model' export
